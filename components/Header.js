3307d02d9ebaa644ee0de451d758e86a8ee69014
Yash_01 <yashagrawal01meh@gmail.com>
2023-07-11 23:34:42 +0530

    Uodated heading

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
   const router = useRouter();
   return (
     <Menu style={{ marginTop: '30px' }}>
-      <Menu.Item onClick={() => router.push('/')}>CrowdCoin</Menu.Item>
+      <Menu.Item onClick={() => router.push('/')}>CrowdWallet</Menu.Item>
       <Menu.Menu position="right">
         <Menu.Item onClick={() => router.push('/')}>Campaigns</Menu.Item>
         <Menu.Item onClick={() => router.push('/campaigns/new')}>+</Menu.Item>
